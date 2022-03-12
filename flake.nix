ba764d42bd5e1a6b072df27c277e235954b00123
hugosenari <hugosenari@gmail.com>
2022-03-12 03:20:02 -0300

    chore: re index mccache

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = ''mccache package: in-memory caching'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."mccache-master".url = "path:./master";
+  inputs."mccache-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mccache-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."mccache-v0_1_1".url = "path:./v0_1_1";
+  inputs."mccache-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mccache-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."mccache-v0_1_2".url = "path:./v0_1_2";
+  inputs."mccache-v0_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mccache-v0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."mccache-v0_2_0".url = "path:./v0_2_0";
+  inputs."mccache-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mccache-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''mccache package: in-memory caching'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-mccache-master.flake = false;
+  inputs.src-mccache-master.owner = "abbeymart";
+  inputs.src-mccache-master.ref   = "refs/heads/master";
+  inputs.src-mccache-master.repo  = "mccache-nim";
+  inputs.src-mccache-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-mccache-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-mccache-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
